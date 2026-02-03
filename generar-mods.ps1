1e80c218a7ad9390332820b5d9fef614f1ce195e
SharKz-Dev <sharkzmc.dev@gmail.com>
2026-02-02 21:45:43 -0300

    Update 1.4.2 Minemon

--- generar-mods.ps1
+++ generar-mods.ps1
@@ -1,20 +1,27 @@
-$baseUrl = "https://sharkz-dev.github.io/minemon.github.io"                                                                                                                                                 
-  Write-Output "=== MODS ==="                                                                                                                                                                              
-  Get-ChildItem -Path ".\mods" -Filter "*.jar" | ForEach-Object {
-      $hash = (Get-FileHash -Algorithm MD5 $_.FullName).Hash.ToLower()
-      $size = $_.Length
-      $name = $_.BaseName
+$baseUrl = "https://sharkz-dev.github.io/minemon.github.io"
+$outputFile = "mods.json"
 
-      Write-Output "{"
-      Write-Output "    `"id`": `"mod:$name`","
-      Write-Output "    `"name`": `"$name`","
-      Write-Output "    `"type`": `"FabricMod`","
-      Write-Output "    `"required`": { `"value`": true, `"def`": true },"
-      Write-Output "    `"artifact`": {"
-      Write-Output "        `"size`": $size,"
-      Write-Output "        `"MD5`": `"$hash`","
-      Write-Output "        `"url`": `"$baseUrl/mods/$($_.Name)`""
-      Write-Output "    }"
-      Write-Output "},"
-      Write-Output ""
-  }
+$items = @()
+
+Get-ChildItem -Path ".\mods" -Filter "*.jar" -File | ForEach-Object {
+    $hash = (Get-FileHash -Algorithm MD5 $_.FullName).Hash.ToLower()
+    $size = $_.Length
+    $name = $_.BaseName
+
+    $items += @{
+        id = "mod:$name"
+        name = $name
+        type = "FabricMod"
+        required = @{
+            value = $true
+            def   = $true
+        }
+        artifact = @{
+            size = $size
+            MD5  = $hash
+            url  = "$baseUrl/mods/$($_.Name)"
+        }
+    }
+}
+
+$items | ConvertTo-Json -Depth 6 | Out-File $outputFile -Encoding UTF8
